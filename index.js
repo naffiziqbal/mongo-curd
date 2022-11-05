68909579acee724eb4db591f09ae4841ee0d6d85
naffiziqbal <f.nafiziqbal@gmail.com>
2022-11-05 11:01:55 +0600

    Adding Service API

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const app = express()
 app.use(cors());
+app.use(express.json())
 const port = process.env.PORT || 5000;
 const users = [
     { id: 1, name: " nafiz", email: " nafiz@g.com" },
@@ -21,12 +22,31 @@ client.connect(err => {
     async function run() {
         try {
             const serviceCollection = client.db('genius-car').collection('services');
+            const orderCollection = client.db('genius-car').collection('Orders');
+
+
             app.get('/services', async (req, res) => {
                 const query = {};
                 const cursor = serviceCollection.find(query);
                 const services = await cursor.toArray();
-                console.log(services);
                 res.send(services)
+            });
+
+            app.get('/services/:id', async(req, res) => {
+                const id = req.params.id;
+                const query = { _id : ObjectId(id)};
+                const service= await serviceCollection.findOne(query);
+            
+                res.send(service)
+            })
+
+            //   Orders api;
+
+            app.post('/orders', async(req, res)=>{
+                const order = req.body;
+                const result =await orderCollection.insertOne(order);
+                res.send(result);
+
             })
 
 
@@ -40,9 +60,13 @@ client.connect(err => {
 });
 
 
-app.get('/users', (req, res) => {
-    res.send(users)
-})
+// app.get('/services/:id', async(req, res) => {
+//     const id = req.params.id;
+//     const query = { _id : ObjectId(id)};
+//     const service= await serviceCollection.findOne(query);
+
+//     res.send(service)
+// })
 
 app.listen(port, () => {
     console.log(` Listening On Port ${port}`);
